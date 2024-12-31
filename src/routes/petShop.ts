e5fbe834db9941c62a0865d1c167ff65fc4052c9
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-12-31 15:12:43 -0300

    feat: adding GET router all for petshops

--- src/routes/petShop.ts
+++ src/routes/petShop.ts
@@ -4,7 +4,15 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const configurePetShopRoutes = (router: Router) => {
-    router.get('/petshops', async (req: Request, res: Response) => {});
+    router.get('/petshops', async (req: Request, res: Response) => {
+        try {
+            const petshops = await prisma.petShops.findMany();
+            res.status(200).json(petshops);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Error retrieving petshops" });
+        }
+    });
 
     router.get('/petshops/:id', async (req: Request, res: Response) => {});
 
