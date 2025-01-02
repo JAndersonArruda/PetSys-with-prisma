d6e9b3bded1ade63adc068395a05ff0a6afb3e00
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-01-01 21:48:10 -0300

    feat: adding GET router all for pets

--- src/routes/pet.ts
+++ src/routes/pet.ts
@@ -4,7 +4,39 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const configurePetRoutes = (router: Router) => {
-    router.get('/pets', async (req: Request, res: Response) => {});
+    router.get('/pets', async (req: Request, res: Response) => {
+        const account = req.headers['cnpj'];
+
+        if (!account) {
+            res.status(401).json({ message: "Missing required header: cnpj" });
+            return;
+        }
+
+        const cnpj = Array.isArray(account) ? account[0] : account;
+
+        try {
+            const userAccount = await prisma.petShops.findFirst({
+                where: {
+                    cnpj: cnpj
+                }
+            })
+    
+            if (!userAccount) {
+                res.status(404).json({ message: "User not exists" });
+            }
+            
+            const pets = await prisma.pets.findMany({
+                where: {
+                    cnpjPetshop: cnpj
+                }
+            });
+
+            res.status(200).json(pets);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Error retrieving pets" });            
+        }
+    });
 
     router.get('/pets/:id', async (req: Request, res: Response) => {});
 
